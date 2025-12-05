99973f61932f8fb0aaea6c2b171e6c80dab71bfd
Sudeep Biswas <sudeepb02@gmail.com>
2025-12-06 00:49:02 +0530

    feat: add yield lock hook with stub functions

---
+++ src/YieldLockHook.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+// V4 imports
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
+import {SwapParams} from "v4-core/types/PoolOperation.sol";
+import {IPoolManager, ModifyLiquidityParams} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {SafeCast} from "v4-core/libraries/SafeCast.sol";
+
+contract YieldLockHook is BaseHook {
+    using PoolIdLibrary for PoolKey;
+    using SafeCast for int256;
+    using BeforeSwapDeltaLibrary for BeforeSwapDelta;
+    using CurrencyLibrary for Currency;
+
+    error InvalidCurrency();
+
+    address public immutable YIELD_TOKEN;
+    address public immutable UNDERLYING_TOKEN;
+
+    constructor(IPoolManager _manager, address _yieldToken, address _underlyingToken) BaseHook(_manager) {
+        YIELD_TOKEN = _yieldToken;
+        UNDERLYING_TOKEN = _underlyingToken;
+    }
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return
+            Hooks.Permissions({
+                beforeInitialize: true, //
+                afterInitialize: false,
+                beforeAddLiquidity: true, //
+                beforeRemoveLiquidity: true, //
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true, //
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: true, //
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    // Required to validate that the hook is being used on the correct pool
+    function _beforeInitialize(address, PoolKey calldata key, uint160) internal override returns (bytes4) {
+        return BaseHook.beforeInitialize.selector;
+    }
+
+    function _beforeAddLiquidity(
+        address,
+        PoolKey calldata key,
+        ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal override returns (bytes4) {
+        return BaseHook.beforeAddLiquidity.selector;
+    }
+
+    function _beforeRemoveLiquidity(
+        address,
+        PoolKey calldata key,
+        ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal override returns (bytes4) {
+        return BaseHook.beforeRemoveLiquidity.selector;
+    }
+
+    function _beforeSwap(
+        address,
+        PoolKey calldata key,
+        SwapParams calldata params,
+        bytes calldata
+    ) internal override returns (bytes4, BeforeSwapDelta, uint24) {
+        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    }
+}
